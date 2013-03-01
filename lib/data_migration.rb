158fb79a3d66f3ee8295d734dd8a4a8cc8d82b57
Alexander Shcherbinin <realmyst@ya.ru>
2013-03-01 13:44:56 +0100

    try to import backtrace bactches

--- lib/data_migration.rb
+++ lib/data_migration.rb
@@ -1,5 +1,7 @@
 require 'rubygems'
 require 'mongo'
+require "activerecord-import/base"
+ActiveRecord::Import.require_adapter('postgresql')
 
 include Mongo
 
@@ -10,7 +12,7 @@ class DataMigration
     config = config.with_indifferent_access
     config[:host] ||= 'localhost'
     config[:port] ||= 27017
-    @cursor_options = {:timeout => false, :batch_size => 1000, :sort => ["_id", "asc"]}
+    @cursor_options = {:timeout => false, :batch_size => 100, :sort => ["_id", "asc"]}
     @mongo_client = MongoClient.new(config[:host], config[:port])
     @db = @mongo_client[config[:database].to_s]
   end
@@ -61,12 +63,24 @@ class DataMigration
       total = old_app["watchers"].count
       log "  Start copy watchers, total: #{total}"
 
+      watchers = []
       old_app["watchers"].each do |watcher|
         log "    copying watcher ##{counter += 1} of #{total} with id #{watcher['_id']}"
 
-        copy_watcher(app, watcher)
+        watchers << copy_watcher(app, watcher)
       end
+      Watcher.import watchers
     end
+  end
+
+  def copy_watcher(app, old_watcher)
+    watcher = app.watchers.new
+    watcher.email = old_watcher["email"]
+    if old_watcher["user_id"]
+      user_id = @user_mapping[old_watcher["user_id"]]
+      watcher.user = User.find(user_id)
+    end
+    watcher
   end
 
   def copy_deploys(old_app, app)
@@ -75,12 +89,25 @@ class DataMigration
       total = old_app["deploys"].count
       log "  Start copy deploys, total: #{total}"
 
+      deploys = []
       old_app["deploys"].each do |deploy|
         log "    copying deploy ##{counter += 1} of #{total} with id #{deploy['_id']}"
 
-        copy_deploy(app, deploy)
+        deploys << copy_deploy(app, deploy)
+        if(deploys.count > 100)
+          Deploy.import deploys
+          deploys = []
+        end
       end
+      Deploy.import deploys
     end
+  end
+
+  def copy_deploy(app, old_deploy)
+    columns = Deploy.columns.map(&:name)
+    deploy = app.deploys.new
+    copy_columns(columns, old_deploy, deploy)
+    deploy
   end
 
   def copy_problems(old_app, app)
@@ -133,26 +160,52 @@ class DataMigration
     end
   end
 
+  def copy_err(problem, old_err)
+    err = problem.errs.new
+    columns = Err.columns.map(&:name).reject {|c| c.in? ["problem_id"]}
+    copy_columns(columns, old_err, err)
+
+    err.save!
+    err
+  end
+
+
   def copy_notices(err, old_err)
     counter = 0
     total = db[:notices].find("err_id" => old_err["_id"]).count
     log "      Start copy notices, total: #{total}"
 
+    notices = []
     db[:notices].find({"err_id" => old_err["_id"]}, @cursor_options) do |cursor|
       cursor.each do |old_notice|
         log "        copying notice ##{counter += 1} of #{total} with id #{old_notice['_id']}"
 
-        copy_notice(err, old_notice)
+        notices << copy_notice(err, old_notice)
+        if(notices.count > 100)
+          Notice.import notices
+          notices = []
+        end
       end
     end
+    Notice.import notices
   end
 
+  def copy_notice(err, old_notice)
+    notice = err.notices.new
+    columns = Notice.columns.map(&:name).reject{|c| c.in?(["err_id", "backtrace_id"])}
+    copy_columns(columns, old_notice, notice)
+
+    notice.backtrace = copy_backtrace(notice, old_notice)
+
+    notice
+  end
 
   def copy_backtrace(notice, old_notice)
     old_backtrace = db[:backtraces].find({"_id" => old_notice["backtrace_id"]}).first
     backtrace = notice.build_backtrace
     columns = Backtrace.columns.map(&:name)
     copy_columns(columns, old_backtrace, backtrace)
+    backtrace.save!
     copy_backtrace_lines(backtrace, old_backtrace)
 
     backtrace
@@ -212,54 +265,6 @@ class DataMigration
       "NotificationService::#{name.demodulize}"
     end
 
-    def copy_watcher(app, old_watcher)
-      watcher = app.watchers.new
-      watcher.email = old_watcher["email"]
-      if old_watcher["user_id"]
-        user_id = @user_mapping[old_watcher["user_id"]]
-        watcher.user = User.find(user_id)
-      end
-      watcher.save!
-      watcher
-    end
-
-    def copy_deploy(app, old_deploy)
-      columns = Deploy.columns.map(&:name)
-      deploy = app.deploys.new
-      copy_columns(columns, old_deploy, deploy)
-      deploy.save!
-      deploy
-    end
-
-    def copy_err(problem, old_err)
-      err = problem.errs.new
-      columns = Err.columns.map(&:name).reject {|c| c.in? ["problem_id"]}
-      copy_columns(columns, old_err, err)
-
-      err.save!
-      err
-    end
-
-    def copy_notice(err, old_notice)
-      notice = err.notices.new
-      columns = Notice.columns.map(&:name).reject{|c| c.in?(["err_id", "backtrace_id"])}
-      copy_columns(columns, old_notice, notice)
-
-      notice.backtrace = copy_backtrace(notice, old_notice)
-
-      notice.save!
-      notice
-    end
-
-    def copy_backtrace_line(backtrace, old_line)
-      line = backtrace.lines.new
-      columns = BacktraceLine.columns.map(&:name)
-      copy_columns(columns, old_line, line)
-
-      line.save!
-      line
-    end
-
     def copy_comment(problem, old_comment)
       comment = problem.comments.new
       columns = ["body", "created_at", "updated_at"]
@@ -283,10 +288,20 @@ class DataMigration
 
     def copy_backtrace_lines(backtrace, old_backtrace)
       if old_backtrace["lines"]
+        lines = []
         old_backtrace["lines"].each do |old_line|
-          copy_backtrace_line(backtrace, old_line)
+          lines << copy_backtrace_line(backtrace, old_line)
         end
+        BacktraceLine.import lines
       end
+    end
+
+    def copy_backtrace_line(backtrace, old_line)
+      line = backtrace.lines.new
+      columns = BacktraceLine.columns.map(&:name)
+      copy_columns(columns, old_line, line)
+
+      line
     end
 
     def copy_columns(columns, copy_from, copy_to)
